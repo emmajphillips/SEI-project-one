e06e821bb595556083f94eef43bcac442b7a188a
emmajphillips <emmjphillips@gmail.com>
2020-04-10 18:18:54 +0100

    generating 10 random bombs at start of game, adding numbers next

--- scripts/app.js
+++ scripts/app.js
@@ -9,6 +9,9 @@ function init() {
   const height = 9
   const cellCount = width * height
 
+  // * Game variables
+  let isPlaying = false
+
   function createGrid() {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
@@ -17,7 +20,20 @@ function init() {
       cells.push(cell)
     }
   }
+
+  function startGame(event) {
+    console.log('clicked', event.target.textContent)
+    if (isPlaying) return
+    isPlaying = true
+    for (let i = 0; i < 10; i++) {
+      cells[Math.floor(Math.random() * cellCount)].classList.add('joe')
+    }
+  }
+  
   createGrid()
+
+  // * Event listeners
+  grid.addEventListener('click', startGame)
 }
 
 window.addEventListener('DOMContentLoaded', init)
