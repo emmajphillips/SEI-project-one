9132038e196de49c904ef7715ae6c6400e1f8f6f
emmajphillips <emmjphillips@gmail.com>
2020-04-12 12:28:36 +0100

    Re-inserted startGame function, need to find way to cover cells

--- scripts/app.js
+++ scripts/app.js
@@ -4,34 +4,41 @@ function init() {
   const grid = document.querySelector('.grid')
   const cells = []
   const joeCount = document.querySelector('#joes-remaining')
-  
+
   // * Grid variables
   const width = 9
   const height = 9
   const cellCount = width * height
-  
+
   // * Game variables
-  
-  // ? Create grid
+  let isPlaying = false
+
   function createGrid() {
+    // ? Create grid
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
+      
       grid.appendChild(cell)
       cells.push(cell)
     }
-    
+  }
+
+  function startGame() {
+    if (isPlaying) return
+    isPlaying = true
     generateBoard()
   }
   
   function generateBoard() {
-    // ? Generate Joe
-    for (let i = 0; i < 10; i++ ) {
+    // ? Generate Joe's and display sum in joeCount
+    // ! BUG: The below does not always generate 10, as there are occasional duplicates with randomIndex variable
+    for (let i = 0; i < 10; i++) {
       const randomIndex = Math.floor(Math.random() * cellCount)
       cells[randomIndex].classList.add('joe')
     }
     joeCount.textContent = (grid.querySelectorAll('.joe')).length
-    
-    // ? Generate numbers in relation to joeCells
+
+    // ? Generate and display numbers within non-Joe cells to indicate in relation to cells with 'joe' class
     cells.forEach((cell, index) => {
       let joeCounter = 0
       const topNeighbour = -width // -9
@@ -65,21 +72,28 @@ function init() {
         joeCounter += 1
       }
       cell.textContent = joeCounter
-      
+
       if (cells[index].hasAttributes()) {
         cell.textContent = ''
       }
     })
+    
   }
 
   function playerMove(event) {
     console.log(event.target)
+    if (event.target.classList.contains('joe')) {
+      alert('You lose')
+    }
   }
 
   createGrid()
 
   // * Event listeners
-  grid.addEventListener('click', playerMove)
+  grid.addEventListener('click', startGame)
+  cells.forEach(cell => {
+    cell.addEventListener('click', playerMove)
+  })
 }
 
 window.addEventListener('DOMContentLoaded', init)
